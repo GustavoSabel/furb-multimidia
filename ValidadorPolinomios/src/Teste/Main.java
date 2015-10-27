17f256b7cded98fecf1bcf7a974352139baa1770
gustavo.sabel <gustavo.sabel.gs@gmail.com>
2015-10-27 13:28:50 -0200

    Ajuste

--- ValidadorPolinomios/src/Teste/Main.java
+++ ValidadorPolinomios/src/Teste/Main.java
@@ -1,7 +1,6 @@
 package Teste;
 
 import java.io.StringReader;
-
 import Validador.LexicalError;
 import Validador.Lexico;
 import Validador.SemanticError;
@@ -10,24 +9,27 @@ import Validador.Sintatico;
 import Validador.SyntaticError;
 
 public class Main {
-	public static void main(String[] args) {
-		try {
-			//String pol1 = "10+2x^2+3x^10";
-			String pol1 = "2-2+10x+x150^2+x(42)x+xx^2x^3";
-			StringReader read = new StringReader(pol1);
-			Lexico lex = new Lexico(read);
-	
-			Sintatico sintatico = new Sintatico();
-			sintatico.parse(lex, new Semantico());
-			
-			System.out.println("É válido!");
-			
-		} catch (LexicalError e) {
-			e.printStackTrace();
-		} catch (SyntaticError e) {
-			e.printStackTrace();
-		} catch (SemanticError e) {
-			e.printStackTrace();
-		}
+
+    public static void main(String[] args) {
+	validar("10+2x^2+3x^10");
+	validar("2-2+10x+x150^2+x(42)x+xx^2x^3");
+    }
+
+    private static void validar(String polinomio) {
+	try {
+
+	    StringReader read = new StringReader(polinomio);
+	    Lexico lex = new Lexico(read);
+	    Sintatico sintatico = new Sintatico();
+	    sintatico.parse(lex, new Semantico());
+
+	    System.out.println(polinomio + " é válido!");
+	} catch (LexicalError e) {
+	    e.printStackTrace();
+	} catch (SyntaticError e) {
+	    e.printStackTrace();
+	} catch (SemanticError e) {
+	    e.printStackTrace();
 	}
+    }
 }
